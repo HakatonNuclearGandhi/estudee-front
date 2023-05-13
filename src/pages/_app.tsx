93152c52b990f4a13ac832c55e005f2c8e2bba17
HJK <supermaster98z@gmail.com>
2023-05-13 13:58:22 +0100

    feature: added initial structure

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { AppProps } from "next/app";
+
+import "@/styles/globals.css";
+
+const queryClient = new QueryClient();
+const App = ({ Component, pageProps }: AppProps) => (
+  <QueryClientProvider client={queryClient}>
+    <Component {...pageProps} />
+  </QueryClientProvider>
+);
+
+export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+const estudee = () => {
+  return (
+    <div>
+      <h1>Estudee</h1>
+    </div>
+  );
+};
+
+export default estudee;
