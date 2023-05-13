1536b0edd84500c5c0e0e8d2e014b438a5db6e35
HJK <supermaster98z@gmail.com>
2023-05-13 22:23:45 +0100

    feature: added color palette

---
+++ src/styles/theme/palette/palette.ts
@@ -0,0 +1,19 @@
+const palette = {
+  darkBlue: {
+    100: "#00004d",
+    200: "#002366",
+    300: "#1034A6",
+  },
+  blue: {
+    100: "#192bc2",
+    200: "#0047ab",
+    300: "#0f52ba",
+  },
+  lightBlue: {
+    100: "#1E90FF",
+    200: "#0197f6",
+    300: "#73c2fb",
+  },
+};
+
+export default palette;
--- src/styles/theme/theme.ts
+++ src/styles/theme/theme.ts
@@ -1,7 +1,9 @@
 import { createTheme } from "@mui/system";
 
+import customPalette from "./palette";
 import customTypography from "./typography";
 
 export const theme = createTheme({
   typography: customTypography,
+  palette: customPalette,
 });
