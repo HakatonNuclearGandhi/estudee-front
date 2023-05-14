1303d46ec6a2b8f340fa6f7bb887229a28c40087
HJK <supermaster98z@gmail.com>
2023-05-14 12:52:01 +0100

    fix: fixed Task.tsx

--- src/common/pages/dashboard-page/common/task-block/task/Task.tsx
+++ src/common/pages/dashboard-page/common/task-block/task/Task.tsx
@@ -1,16 +1,15 @@
 import { FC } from "react";
+import { Box, Chip, Typography } from "@mui/material";
+
+import { wrapper } from "./Task.styles";
 
 export interface TaskProps {
   subject: string;
   task: string;
-  deadline: string;
-  mark: string;
+  deadline?: string;
+  mark?: string;
   status: string;
 }
-
-import { Box, Chip, Typography } from "@mui/material";
-
-import { wrapper } from "./Task.styles";
 
 const Task: FC<TaskProps> = ({ subject, task, deadline, mark, status }) => (
   <Box sx={wrapper}>
